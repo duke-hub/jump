035c7a45537ac23eb5e9d777ff8047160a71d5b7
miaomiaodawang123 <linxinyu1998123@163.com>
2023-12-13 10:29:58 +0800

    baseCommit

---
+++ product_services/src/main/java/com/it/cast/Application.java
@@ -0,0 +1,16 @@
+package com.it.cast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author：xxm
+ * @date 2023/12/13 9:56 上午
+ * Description: base
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
